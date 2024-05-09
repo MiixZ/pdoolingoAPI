a16d56c1f37e5cfa4efef5f104a6b01bf5db00a0
isma <xwarzmiixz@gmail.com>
2024-05-09 23:04:08 +0200

    Scripts y dependencias de inicialización

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT ?? 3000;
+
+app.use(express.json());
 
 // Rutas
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("¡Hola, mundo!");
 });
 
