4d3d4ffe9ebee6431c6f1a7663307c54f835d5ea
isma <xwarzmiixz@gmail.com>
2024-05-09 23:21:23 +0200

    Inicio database

---
+++ src/database/database.ts
@@ -0,0 +1,36 @@
+import mysql2, {
+  ConnectionOptions,
+  ResultSetHeader,
+  RowDataPacket,
+} from "mysql2/promise";
+
+const config: ConnectionOptions = {
+  // Cambiar a las opciones del departamento.
+  host: "localhost",
+  user: "root",
+  port: 3306,
+  password: "1111",
+  database: "pdoolingodb",
+  connectionLimit: 20,
+  waitForConnections: true,
+  queueLimit: 0,
+};
+
+const pool = mysql2.createPool(config);
+
+class Database {
+  async query<T extends RowDataPacket[] | ResultSetHeader[]>(
+    sql: string,
+    values?: any
+  ) {
+    const connection = await pool.getConnection();
+    try {
+      const [results] = await connection.query<T>(sql, values);
+      return results;
+    } finally {
+      connection.release();
+    }
+  }
+}
+
+export default new Database();
