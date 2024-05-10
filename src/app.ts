20c0868740c71183b1827c0ee3e7f2324423904c
isma <xwarzmiixz@gmail.com>
2024-05-10 11:27:39 +0200

    Probadas todas las tablas para las respuestas

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import db from "./database/database";
 
 const app = express();
 const port = process.env.PORT ?? 3000;
@@ -6,8 +7,10 @@ const port = process.env.PORT ?? 3000;
 app.use(express.json());
 
 // Rutas
-app.get("/", (req: Request, res: Response) => {
-  res.send("¡Hola, mundo!");
+app.get("/", async (req: Request, res: Response) => {
+  //res.send("¡Hola, mundo!");
+  const results = await db.query("SELECT * FROM USUARIOS_EJERCICIOS");
+  res.json(results);
 });
 
 // Iniciar el servidor
